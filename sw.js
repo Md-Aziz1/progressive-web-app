0a892ee156f5f635a1f176a16bc38358140a9259
Md-Aziz1 <astc2357@gmail.com>
2024-07-08 22:21:53 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ self.addEventListener("install", e => {
 });
 
 self.addEventListener("fetch", e => {
-    e.repondWith(
+    e.respondWith(
         caches.match(e.request).then(response => {
             return response || fetch(e.request);
         })
