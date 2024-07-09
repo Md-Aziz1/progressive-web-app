3cf51ba06d5fb29cc0f70c4d1f9252fdf55c9d46
Md-Aziz1 <astc2357@gmail.com>
2024-07-09 09:17:31 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,27 @@
 self.addEventListener("install", e => {
     e.waitUntil(
         caches.open("static").then(cache => {
-            return cache.addAll(["./","./src/master.css", "./images/logo192.png"]);
+            return cache.addAll(["./", "./src/master.css", "./images/logo192.png"]);
         })
     );
 });
 
 self.addEventListener("fetch", e => {
     e.respondWith(
-        caches.match(e.request).then(response => {
-            return response || fetch(e.request);
-        })
-    )
+        fetch(e.request)
+            .then(response => {
+                // If the response is successful, update the cache
+                if (response.status === 200) {
+                    const responseClone = response.clone();
+                    caches.open("static").then(cache => {
+                        cache.put(e.request, responseClone);
+                    });
+                }
+                return response;
+            })
+            .catch(() => {
+                // If the network request fails, serve from cache
+                return caches.match(e.request);
+            })
+    );
 });
